0dda9055b2cbb41e7016ef55418e654ccf9c6e15
adkozlovskiy <whiititi@gmail.com>
2021-04-13 12:08:37 +0300

    Date condition fixed.

--- src/main/java/kozlovskiy/prod/repo/SessionRepo.java
+++ src/main/java/kozlovskiy/prod/repo/SessionRepo.java
@@ -13,7 +13,7 @@ public interface SessionRepo extends JpaRepository<Session, Long> {
      * @param id the movie id
      * @return List of {@link Session} WHERE movie_id = id AND session_date > current date
      */
-    @Query(value = "SELECT * FROM sessions WHERE movie_id = :movieId AND session_date > CURRENT_DATE() ORDER BY session_date", nativeQuery = true)
+    @Query(value = "SELECT * FROM sessions WHERE movie_id = :movieId AND session_date > CURRENT_TIMESTAMP() ORDER BY session_date", nativeQuery = true)
     List<Session> findByMovieId(@Param("movieId") Long id);
 
     /**
@@ -21,6 +21,6 @@ public interface SessionRepo extends JpaRepository<Session, Long> {
      * @return {@link Session} WHERE session_id = id AND session_date > current date
      * Used to figure out if the session is available.
      */
-    @Query(value = "SELECT * FROM sessions WHERE id = :session_id AND session_date > CURRENT_DATE()", nativeQuery = true)
+    @Query(value = "SELECT * FROM sessions WHERE id = :session_id AND session_date > CURRENT_TIMESTAMP()", nativeQuery = true)
     Session getSession(@Param("session_id") Long id);
 }
